7dcea4322b44bff0ab35bb3d38ad9861f6eb5a96
Konstantin Poklonskii <k.poklonskii@dodopizza.com>
2022-06-10 17:34:46 +0200

    cleanup

--- tests/algo.spec.ts
+++ tests/algo.spec.ts
@@ -2,13 +2,13 @@ import { Algo } from '../src/algo'
 
 test('Should return empty array when bitmap is empty', () => {
   let algo = new Algo();
-  var result = algo.findDistanceOfNearestWhiteCell([]);
+  let result = algo.findDistanceOfNearestWhiteCell([]);
   expect(result).toEqual([]);
 })
 
 test('Should return 0 when bitmap with single cell 1', () => {
   let algo = new Algo();
-  var result = algo.findDistanceOfNearestWhiteCell(
+  let result = algo.findDistanceOfNearestWhiteCell(
     [[1]]
   );
   expect(result).toEqual([[0]]);
@@ -16,7 +16,7 @@ test('Should return 0 when bitmap with single cell 1', () => {
 
 test('Should return int.Max when bitmap with single cell 0', () => {
   let algo = new Algo();
-  var result = algo.findDistanceOfNearestWhiteCell(
+  let result = algo.findDistanceOfNearestWhiteCell(
     [[0]]
   );
   expect(result).toEqual([[Number.MAX_SAFE_INTEGER]]);
@@ -25,7 +25,7 @@ test('Should return int.Max when bitmap with single cell 0', () => {
 
 test('Should return all 0 when all cells in bitmap are black', () => {
   let algo = new Algo();
-  var result = algo.findDistanceOfNearestWhiteCell(
+  let result = algo.findDistanceOfNearestWhiteCell(
     [[1,1,1],
      [1,1,1]]
   );
@@ -35,7 +35,7 @@ test('Should return all 0 when all cells in bitmap are black', () => {
 
 test('Should return correct answer for control test case', () => {
   let algo = new Algo();
-  var result = algo.findDistanceOfNearestWhiteCell(
+  let result = algo.findDistanceOfNearestWhiteCell(
     [[0,0,0,1],
      [0,0,1,1],
      [0,1,1,0]]
